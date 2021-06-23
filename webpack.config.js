1112fef0068147c089596f9b4c47da56c28dca89
Nikita Dmitrienko <nikita.d@thrive.io>
2021-06-23 11:33:05 +0300

    add development failsafe env

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ module.exports = {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
   },
-  mode: process.env.NODE_ENV,
+  mode: process.env.NODE_ENV || 'development',
   module: {
     rules: [
       {
